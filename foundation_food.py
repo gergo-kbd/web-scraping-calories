04c108ad3806aaa7ca552fb04fd5f04f18f92670
=gergo-kbd <kelemen.gergo99@gmail.com>
2025-04-05 14:19:52 +0200

    ignore apikey.txt

--- foundation_food.py
+++ foundation_food.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
-api_key = ""  # Ide írd be az API kulcsodat
+
+with open("api_key.txt", "r") as file:
+    api_key = file.read().strip()
+
+
 search_term = "rice, raw"  # Példa keresési kifejezés
 api_url = f"https://api.nal.usda.gov/fdc/v1/foods/search?api_key={api_key}&query={search_term}&dataType=Foundation"
 
@@ -11,7 +15,7 @@ if response.status_code == 200:
     data = response.json()
     if data['foods']:
         for food in data['foods']:
-            if food.get('dataType') == 'Foundation': #Csak a foundation adatokat jelenítjük meg.
+            if food.get('dataType') == 'Foundation': # only foundation
                 print(json.dumps(food, indent=4)) #Teljes food adat kiírása.
                 # Itt kinyerheted a tápértékadatokat
                 break #Megtaláltuk a foundatiot, nincs értelme tovább iterálni.

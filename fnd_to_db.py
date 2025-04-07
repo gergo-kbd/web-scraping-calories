f90f939840e64fff41fe24ea2a7d3c080e7ed1b5
=gergo-kbd <kelemen.gergo99@gmail.com>
2025-04-07 13:38:13 +0200

    fnd_to_db.py is fixed, properly creates a .db TODO: check if table relations are valid or it can be crated or not

--- fnd_to_db.py
+++ fnd_to_db.py
@@ -16,31 +16,38 @@ except json.JSONDecodeError:
     print(f"Error: Could not decode JSON from the file '{file_path}'. Please ensure it's valid JSON.")
     exit()
 
-# Normalize the main food data
-df_foods = pd.json_normalize(json_data)
+print(json_data.keys())
+#print(json.dumps(json_data, indent=4, sort_keys=True))  # Formatted print
 
-# Normalize 
+# Normalize
+df_foods = pd.json_normalize(json_data, record_path='FoundationFoods', errors='ignore')
+df_nutrients = pd.json_normalize(json_data, record_path=['FoundationFoods', 'foodNutrients'], meta=['description'], errors='ignore')
+df_portions = pd.json_normalize(json_data, record_path=['FoundationFoods', 'foodPortions'], meta=['description'], errors='ignore')
 
-df_nutrients = pd.json_normalize(json_data, record_path='FoundationFoods', meta=['id', 'description'])
+print(df_foods.info())
 
-df_nutrients = pd.json_normalize(json_data, record_path='foodNutrients', meta=['id', 'description'])
-df_portions = pd.json_normalize(json_data, record_path='foodPortions', meta=['id', 'description'])
+conn = sqlite3.connect('foundation_foods.db') 
 
+# A df_foods DataFrame oszlopainak konvertálása és törlése
+for col in df_foods.columns:
+    if col in ['description', 'publicationDate', 'foodCategory.description']:
+        try:
+            df_foods[col] = df_foods[col].astype(str)
+        except:
+            print(f"Nem sikerült konvertálni a {col} oszlopot a df_foods DataFrame-ben.")
+    elif col in ['foodNutrients', 'foodAttributes', 'nutrientConversionFactors', 'foodPortions', 'inputFoods']:
+        df_foods = df_foods.drop(columns=[col])
 
-conn = sqlite3.connect('my_database.db') # Creates the file if it doesn't exist
-
+#'foods', 'foodNutrients', 'foodPortions' write to sql table
 table_name_foods = 'foods_table'
 df_foods.to_sql(table_name_foods, conn, if_exists='replace', index=False)
-
 
 table_name_nutrients = 'food_nutrients_table'
 df_nutrients.to_sql(table_name_nutrients, conn, if_exists='replace', index=False)
 
-
 table_name_portions = 'food_portions_table'
 df_portions.to_sql(table_name_portions, conn, if_exists='replace', index=False)
 
-# Close the connection
 conn.close()
 
-print(f"The JSON data from '{file_path}' has been successfully converted to '{table_name_foods}', '{table_name_nutrients}', and '{table_name_portions}' tables in the 'my_database.db' database.")
+print(f"'{file_path}' Success json -> sql table creation '{table_name_foods}' '{table_name_nutrients}' '{table_name_portions}' tables have been created -> foundation_food.db .")

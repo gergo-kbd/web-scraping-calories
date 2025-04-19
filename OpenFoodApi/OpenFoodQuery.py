0d28b188e6c1431e22ced0d2001f7a841dbc1e39
=gergo-kbd <kelemen.gergo99@gmail.com>
2025-04-19 13:57:45 +0200

    added sort_by, tagtype, tag_contains options to openfoodquery search

--- OpenFoodApi/OpenFoodQuery.py
+++ OpenFoodApi/OpenFoodQuery.py
@@ -59,7 +59,15 @@ class OpenFoodQuery:
         except OpenFoodFactsAPIError as e:
             raise
 
-    def search_products(self, search_term: str, page: int = 1, page_size: int = 20) -> Dict:
+    def search_products(
+        self,
+        search_term: str,
+        page: int = 1,
+        page_size: int = 20,
+        sort_by: Optional[str] = None,
+        tagtype_0: Optional[str] = None,
+        tag_contains_0: Optional[str] = None,
+    ) -> Dict:
         
         endpoint = "/cgi/search.pl"
 
@@ -73,4 +81,21 @@ class OpenFoodQuery:
             "fields": "code,product_name,brands,quantity,nutriments,image_front_small_url", # Specify fields to return
             "lc": self.language  # Language code
         }
+
+        if sort_by:
+            params["sort_by"] = sort_by
+        
+        if tagtype_0 and tag_contains_0:
+            params["tagtype_0"] = tagtype_0
+            params["tag_contains_0"] = tag_contains_0
+
+        '''
+        sort_by (Optional[str]): The field to sort the results by (e.g., "popularity").
+                                 Check the API documentation for valid values.
+        tagtype_0 (Optional[str]): The type of tag to filter by (e.g., "allergens").
+                                   Check the API documentation for valid values.
+        tag_contains_0 (Optional[str]): Whether the product contains the tag ("contains")
+                                        or not ("does_not_contain").
+                                        Check the API documentation for valid values.
+        '''
         return self._get(endpoint, params)

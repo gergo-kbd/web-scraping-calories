77e44372483d8e16d2d3173329db2b5850044cf8
=gergo-kbd <kelemen.gergo99@gmail.com>
2025-04-30 23:43:16 +0200

    fixed FoodApp.py, now backends accepts the request of frontend

--- FoodApp.py
+++ FoodApp.py
@@ -3,8 +3,21 @@ from OpenFoodApi.models import *
 from usdaQuery.usda_food_query import *
 from usdaQuery.models import *
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # vagy: ["*"] ha mindent engednél
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 with open("api_key.txt", "r") as file:
         API_KEY = file.read().strip()

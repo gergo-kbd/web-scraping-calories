67ae67ee199a04b59106ee24361400e3f63b11f0
=gergo-kbd <kelemen.gergo99@gmail.com>
2025-05-01 17:21:14 +0200

    deleted frontend folder, react prj was badly initiated

--- FoodApp.py
+++ FoodApp.py
@@ -13,7 +13,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,            # vagy: ["*"] ha mindent engednél
+    allow_origins=origins,           
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

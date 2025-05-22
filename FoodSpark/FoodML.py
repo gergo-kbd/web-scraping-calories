0b93649335d7d182ce5e0551f42a1d375431680f
=gergo-kbd <kelemen.gergo99@gmail.com>
2025-05-22 23:53:51 +0200

    added ML functions

--- FoodSpark/FoodML.py
+++ FoodSpark/FoodML.py
@@ -1,44 +1,14 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, size, col
-import json
+from pyspark.ml.feature import VectorAssembler, StringIndexer
+from pyspark.ml import pipeline
 
-#Saprksession starts here
-spark = SparkSession.builder.appName("FoodML").getOrCreate()
+label_indexer = StringIndexer(inputCol="foodCategory",outputCol= "label")
 
-#read json
-usda_fnds_json = spark.read.format('json').option('multiline', 'true').load("FoodData_Central_foundation_food_json_2025-04-24.json")
+nutrient_cols =[col for col in df_final.columns if col not in["fdcId", "description", "foodCategory"]]
 
-# checking the schema
-#usda_fnds_json.printSchema()
+assembler = VectorAssembler(inputCols=nutrient_cols, outputCol="features")
 
-df_foods = usda_fnds_json.select(explode("foods").alias("food"))
+#pipeline
+pipeline = pipeline(stages =[label_indexer, assembler])
 
-# explode nutrients
-df_nutrients = df_foods.select(
-    "food.fdcId",
-    "food.description",
-    explode("food.foodNutrients").alias("nutrient")
-)
-
-# only nutrient name and value, and dimension is needed
-df_nutrients =  df_nutrients.select(
-    "fdcId",
-    "description",
-    "foodCategory",
-    col("nutrient.nutrientName").alias("nutrient_name"),
-    col("nutrient.value").alias("nutrient_value"),
-    col("nutrient.unitName").alias("nutrient_unit")
-)
-
-# pivot
-df_nutrients_pivot = df_nutrients.groupBy("fdcId", "description", "foodCategory")\
-    .pivot("nutrient_name")\
-    .agg(F.first("nutrient_value"))
-
-df_final = df_nutrients_pivot.fillna(0)
-
-df_final.show(5)
-
-
-
-
+df_rdy = pipeline.fit(df_final).transform(df_final)
+df_rdy.select("label", "features").show(truncate=False)
